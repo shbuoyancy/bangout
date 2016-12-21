c77724428a77b281897b4206522439c6449d5d28
Jacksing <hijackyeah@163.com>
2016-12-21 18:49:05 +0800

    Update terrain.py

--- tests/support/terrain.py
+++ tests/support/terrain.py
@@ -1,1 +1,33 @@
-    world.host = 'http://106.14.41.253'
+from selenium import webdriver
+
+from lettuce import before, after, world
+
+from pyutils.ie_clear import clear_cookies
+
+
+@before.all  # pylint: disable=no-member
+def setup_host():
+    world.host = ''
+    # world.path_mappings = {}
+
+
+@before.all  # pylint: disable=no-member
+def setup_browser():
+    world.browser = webdriver.Ie()
+
+
+@before.all  # pylint: disable=no-member
+def clear_browser():
+    if isinstance(world.browser, webdriver.Ie):
+        clear_cookies()
+
+
+@before.each_scenario  # pylint: disable=no-member
+def clear_session(scenario):
+    world.browser.delete_all_cookies()
+
+
+@after.all  # pylint: disable=no-member
+def close_browser(total):
+    world.browser.delete_all_cookies()
+    world.browser.close()
